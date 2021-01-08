777425948ca005b9ff65ac93f1a7e446db5a1ffa
Mathew E Newton <elijah2895@hotmail.com>
2021-01-08 01:04:55 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,10 +3,9 @@
 board[4] = "X"
 display_board(board)
 
-def display_board(board)
-  puts "#{board[0]} | #{board[1]} | #{board[2]}"
-  puts "-----------"
-  puts "#{board[0]} | #{board[1]} | #{board[2]}"
-  puts "-----------"
-  puts "#{board[0]} | #{board[1]} | #{board[2]}"
-end
+
+puts "#{board[0]} | #{board[1]} | #{board[2]}"
+puts "-----------"
+puts "#{board[0]} | #{board[1]} | #{board[2]}"
+puts "-----------"
+puts "#{board[0]} | #{board[1]} | #{board[2]}"
