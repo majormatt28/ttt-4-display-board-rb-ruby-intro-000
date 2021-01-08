e5854fb65e4a9378671a8dbcfd6f5da3c0580da9
Mathew E Newton <elijah2895@hotmail.com>
2021-01-08 00:36:00 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,12 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board (board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
   puts ["#{board[0]}"]
   puts ["#{board[1]}"]
   puts ["#{board[2]}"]
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
 end
