4366aa781bc8bf1143731187675a9e0b4e4118ea
Mathew E Newton <elijah2895@hotmail.com>
2021-01-08 01:01:56 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board[4] = "X"
+
 def display_board(board)
   puts "#{baord[0]} | #{board[1]} | #{board[2]}"
   puts "-----------"
